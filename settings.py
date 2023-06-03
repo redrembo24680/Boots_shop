4ca4fc696bfd59cd171c40869bd0d5d0bc530af7
romanchy <nicroma014@gmail.com>
2023-06-03 18:20:48 +0300

    first comit

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    DATABASE='internet_shop'
+    USER='postgres'
+    PASSWORD='roman1201'
+    HOST='127.0.0.1'
+    PORT='5432'
+    API_TOKEN='6226396360:AAFPDVmZsjJ5c8cfFt2Ya7gBg7KU_dsUJko'
